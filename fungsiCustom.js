9b1331cab33aedaa571c61664b9fc0aab31620ca
thisanam <88670549+thisanam@users.noreply.github.com>
2022-06-10 08:52:29 +0700

    Update fungsiCustom.js
    done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,46 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+
+const dataGenerator = (rawData) => {
+
+  // Fungsi untuk mengambil kata terakhir dari pesan yg sudah dibaca
+  // Setiap kondisi spesifik dengan setiap file yang akan dibaca
+
+  if (rawData.message !== undefined) {
+    let processedData = rawData.message.split(" ");
+    return processedData[processedData.length - 1];
+  }
+
+  if (rawData[0].message !== undefined) {
+    let processedData = rawData[0].message.split(" ");
+    return processedData[processedData.length - 1];
+  }
+
+  if (rawData[0].data.message !== undefined) {
+    let processedData = rawData[0].data.message.split(" ");
+    return processedData[processedData.length - 1];
+  }
+};
+
+
+async function bacaData(fnCallback) {
+  let fileList = [file1, file2, file3];
+  let data = [];
+
+  try {
+    // Menggunakan for of, karena await tidak akan bs berjalan apabila menggunakan for each.
+    for (const e of fileList) {
+      const readedData = await fs.promises.readFile(e, "utf-8");
+      // menggunakan fungsi dataGenerator untuk mendapatkan kata terakhir.
+      data.push(dataGenerator(JSON.parse(readedData)));
+    }
+    fnCallback(null, data);
+  } catch (err) {
+    fnCallback(err, null);
+  }
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
